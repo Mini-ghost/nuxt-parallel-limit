1bbc5bf8aa0c783c2b35b865f1e4f2bccae07647
Alex Liu <dsa1314@gmail.com>
2023-08-05 22:18:22 +0800

    chore: add husky and lint-staged

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,5 @@
-import {
-  addPlugin,
-  addTemplate,
-  createResolver,
-  defineNuxtModule,
-} from "@nuxt/kit";
-import { resolve } from "pathe";
+import { addPlugin, addTemplate, createResolver, defineNuxtModule } from '@nuxt/kit';
+import { resolve } from 'pathe';
 
 export interface ParallelLimitPattern {
   pattern: string;
@@ -17,7 +12,7 @@ export interface ModuleOptions {
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    configKey: "parallelLimit",
+    configKey: 'parallelLimit',
   },
   async setup(options, nuxt) {
     if (!options || !Array.isArray(options.patterns)) return;
@@ -25,16 +20,16 @@ export default defineNuxtModule<ModuleOptions>({
     const resolver = createResolver(import.meta.url);
 
     // Inject options via virtual template
-    nuxt.options.alias["#parallel-limit-options"] = addTemplate({
-      filename: "parallel-limit-options.mjs",
+    nuxt.options.alias['#parallel-limit-options'] = addTemplate({
+      filename: 'parallel-limit-options.mjs',
       getContents: () => `export const options = ${JSON.stringify(options)};`,
     }).dst;
 
-    const runtimeDir = await resolver.resolve("./runtime");
+    const runtimeDir = await resolver.resolve('./runtime');
     nuxt.options.build.transpile.push(runtimeDir);
 
     // Add plugins
-    for (const template of ["plugin"]) {
+    for (const template of ['plugin']) {
       addPlugin(resolve(runtimeDir, template));
     }
   },
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,29 +1,27 @@
-import pLimit from "p-limit";
+import pLimit from 'p-limit';
 
-import { defineNuxtPlugin } from "#imports";
-import { options } from "#parallel-limit-options";
+import { defineNuxtPlugin } from '#imports';
+import { options } from '#parallel-limit-options';
 
-import type { $Fetch, NitroFetchRequest } from 'nitropack'
+import type { $Fetch, NitroFetchRequest } from 'nitropack';
 
 const limitMap = new Map<string, ReturnType<typeof pLimit>>();
 
 export default defineNuxtPlugin(() => {
-  const $fetch = globalThis.$fetch as $Fetch<unknown, NitroFetchRequest> & { _limit?: boolean };
-  if (process.server && $fetch._limit) return
+  const $fetch = globalThis.$fetch as $Fetch<unknown, NitroFetchRequest> & {
+    _limit?: boolean;
+  };
+  if (process.server && $fetch._limit) return;
 
   const { patterns } = options;
   if (!patterns.length) return;
 
-  globalThis.$fetch = ((
-    ...args: Parameters<typeof $fetch>
-  ): ReturnType<typeof $fetch> => {
+  globalThis.$fetch = ((...args: Parameters<typeof $fetch>): ReturnType<typeof $fetch> => {
     const [request] = args;
 
     const url = request instanceof Request ? request.url : request;
 
-    const pattern = patterns.find(({ pattern }) =>
-      new RegExp(pattern).test(url),
-    );
+    const pattern = patterns.find(({ pattern }) => new RegExp(pattern).test(url));
 
     if (!pattern) return $fetch(...args);
 
@@ -32,16 +30,16 @@ export default defineNuxtPlugin(() => {
       limitMap.set(pattern.pattern, (limit = pLimit(pattern.limit)));
     }
 
-    return limit(() => $fetch(...args)
-      .finally(() => {
-        if(limit?.pendingCount === 0) limitMap.delete(pattern.pattern);
-      }));
+    return limit(() =>
+      $fetch(...args).finally(() => {
+        if (limit?.pendingCount === 0) limitMap.delete(pattern.pattern);
+      })
+    );
   }) as typeof $fetch;
 
   if (process.server) {
-    $fetch._limit = true
+    $fetch._limit = true;
   }
 
   Object.assign(globalThis.$fetch, $fetch);
 });
-
